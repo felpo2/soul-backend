389c1b3b25310cf52d525b291882e12b5a0d27b1
Felipe <fthomaz100@gmail.com>
2026-05-06 10:33:52 -0300

    primeiro commit

---
+++ src/main/java/com/project/soul/SoulApplication.java
@@ -0,0 +1,13 @@
+package com.project.soul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoulApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/soul/SoulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.soul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
